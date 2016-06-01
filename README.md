e3d71fdaf923169ca3eb82a0898c8ac1103af286
Doga Ulupinar <doga.ulupinar@engineering.ucla.edu>
2016-06-01 14:56:51 -0700

    Combine the mixture model into the main script and add gitignore

--- README.md
+++ README.md
@@ -1,31 +1,23 @@
-## Synopsis
+## Project 4 ancestry mapping
 
-At the top of the file there should be a short introduction and/ or overview that explains **what** the project is. This description should match descriptions added for package managers (Gemspec, package.json, etc.)
+I did project 4, ancestry mapping for the hard difficulty of determining the global ancestry from unknown populations. Here you will find all the code for the project in final224.R and the presentation i gave in discussion Friday May 20th.
 
-## Code Example
+## Approach
 
-Show what the library does as concisely as possible, developers should be able to figure out **how** your project solves their problem by looking at the code example. Make sure the API you are showing off is obvious, and that your code is short and concise.
+I used the unsupervised clustering machine learning technique, kmeans to cluster the indivudals into k different populations where our k is 4 {Asian,European,American,African}. In order to do so i needed to reduce the dimensionality of the data. This was accomplished using Principal Component Analysis (PCA) and accounting for LD. I took snp out of every 15 (this was according to data i found online for the size of a typical LD block) and on this reduced dataset i ran a PCA. Looking at the first few PC's i was able to classify the data quite well. This process was repeated for chromosomes 22 ,21 and 20 using the 1000 genomes data set. The PCA's from the different chromosomes can be combine and the kmeans can be run over these new clusters. For more information and graphics please refer to the presentation
 
-## Motivation
+## Testing
 
-A short description of the motivation behind the creation and maintenance of the project. This should explain **why** the project exists.
+A k-fold cross validation sheme was used to assess the classification done by my method. I used 10 folds where i used divided the data from each population into to 10 slices and trained on the other 9 and predicted the remaining 1 slice. Accuracy improved with each addition of each new chromosome PCA data set.
 
-## Installation
+## Additional Exploratory work - Sparse PCA
 
-Provide code examples and explanations of how to get the project.
+I performed a Sparse PCA to determine the min # of SNPs required (and which snps were required) to get the same classification accuracy as working on the entire Chromosome. Then using these reduced set of SNPS we can try to pinpoint which genes are most expressive in determining the population structure in a group of individuals 
 
-## API Reference
+## Additional Exploratory work - Mixture Modeling
 
-Depending on the size of the project, if it is small and simple enough the reference docs can be added to the README. For medium size to larger projects it is important to at least provide a link to where the API reference docs live.
+In addition i implemented a simple mixture model and ran it on the reduced SNPs data set. It is a simplified version of admixture modeling where I state that the population for an individual is selected from the multinomial distribution governed how the prior P_pop of the percentatge of study individuals from each population.A given SNP for a given individual was drawn using the Bernoiulli distrbution dependent on parameter F, the allele frequency of that gene at that population. I learned the parameters F and P_pop using expectation maximization algorithim. The results were unfortunately pretty bad, with an F1 score of .741 where when kmeans was applied on the same data we get an F1score of  7.45
 
-## Tests
+## The code set up
 
-Describe and show how to run the tests with code examples.
-
-## Contributors
-
-Let people know how they can dive into the project, include important links to things like issue trackers, irc, twitter accounts if applicable.
-
-## License
-
-A short snippet describing the license (MIT, Apache, etc.)
+The code is set up into different regions to perform different tasks. The entire script is not meant to be run all at once. Please refer to the comments in the code to run what you desire.Split into three major sections, function implementations, data initialization, and all machine learning tasks(most of the project here is calling the function i wrote and benchmarking)
--- final224.R
+++ final224.R
@@ -9,9 +9,77 @@ library(nsprcomp)
 library(factoextra)
 library(clue)
 library(scatterplot3d)
+### THE FUNCTIONS ###
 
-progress_string = "Total Time it took to %s was: %s seconds"
+#E-step function- Performs the expecation step for the EM algorthim for mixture modeling
+#it uses current iteration parameters to recompute the missing data which in our case is
+#the parameter that indicates which population each individual is from
+E.step = function(Xij,Fjk,PIk,Rik,K){
+  eps = 1e-6
+  num_individuals = dim(Rik)[1]
+  num_snps = dim(Fjk)[1]
+  for (k in 1:K){
+    for ( i in 1:num_individuals){
+      if (i %% 100 == 0) print(i)
+      sum_indiv = 0
+      for (j in 1:num_snps){
+        #log probability
+        sum_indiv = sum_indiv + (1 - Xij[i,j])*log(1-Fjk[j,k]) + Xij[i,j]*log(Fjk[j,k])
+      }
+      Rik[i,k] = sum_indiv + log(PIk[1,k])
+    }
+  }
+  
+  #using log sum exp trick
+  likelihood = sum(apply(Rik, 1, max))
+  
+  #Change back to likelihood
+  maxi = matrix(apply(Rik,1,max))
+  Rik = sweep(Rik,1,maxi,'-')
+  Rik = exp(Rik)
+  Rik = Rik + eps
+  Rik = sweep(Rik,1,matrix(apply(Rik,1,sum)),'/')
+  
+  return(list( Rik = Rik,likelihood = likelihood)) #normailize by sum of each row (k populations)
+}
 
+#Mixture function which runs a mixture model on a population given the matrix of individuals by SNPS
+# and the expected number of populations
+mixture = function(Xij,K){
+  #intialize data
+  num_snps = ncol(Xij)
+  num_individuals = nrow(Xij)
+  
+  Fjk  = matrix(runif(num_snps*K),num_snps,K)  #MxK
+  PIk  = matrix(1/K,1,K)                              #1xK
+  
+  Rik  = matrix(runif(num_individuals*K),num_individuals,K)
+  
+  
+  
+  old_likelihood = -Inf 
+  likelihood = 0
+  while (abs(abs(likelihood) - abs(old_likelihood)) > 1){
+    old_likelihood = likelihood
+    
+    ## E-step
+    My_vars = E.step(Xij,Fjk,PIk,Rik,K)
+    likelihood = My_vars$likelihood
+    
+    ## M-Step
+    # PIk
+    PIk  = t(matrix(apply(My_vars$Rik,2,mean)))
+    
+    # Fjk
+    Fjk =  sweep(t(Xij) %*% My_vars$Rik,2,t(matrix(apply(My_vars$Rik,2,sum))),'/')
+    print(My_vars$likelihood)
+    
+  }
+  return(list(Rik = My_vars$Rik,PIk = PIk,Fjk = Fjk))
+}
+
+
+#adds label data to graphs
 colLab <- function(n) {
   if (is.leaf(n)) {
     a <- attributes(n)
@@ -52,6 +120,7 @@ F1score <- function(cluster,populations,verbosity = 0){
   return(F1_score_total/length(distinct_populations))
 }
 
+### INITALIZE ALL THE DATA ###
 CHROMOSOME_NUMBER = "22"
 if ("data_init" %in% ls() == FALSE){
   task = "read in data"
@@ -67,10 +136,15 @@ if ("data_init" %in% ls() == FALSE){
   num_snps = dim(snps)[1]
   snp_id = paste("snp",str_split_fixed(snps$V1, ":", 2)[,2],sep = "")
   diploid = haploid[1:num_snps,seq(1,num_individuals,2),with = FALSE] + haploid[1:num_snps,seq(2,num_individuals,2), with = FALSE]
+  half_hap = haploid[1:num_snps,seq(1,num_individuals,2),with = FALSE]
+  
   colnames(diploid) = as.character(individuals[seq(1,num_individuals,2),1])
   rownames(diploid) = snp_id
   
+  colnames(half_hap) = as.character(individuals[seq(1,num_individuals,2),1])
+  rownames(half_hap) = snp_id
   
+  half_hap = t(half_hap)
   diploid = t(diploid)
   data_init = 1
   total_time = Sys.time() - start_readin
@@ -79,7 +153,7 @@ if ("data_init" %in% ls() == FALSE){
   rm(haploid)
 }
 
-INTERVAL = 15
+INTERVAL = 15 #based on the LD that is present according to online research
 
 #split up data
 global_populations = str_split_fixed(individuals[seq(1, num_individuals, 2), 3], ":", 2)[,2]
@@ -88,22 +162,21 @@ rows_wanted = which(global_populations != "AFR")
 pop_reduce = global_populations[rows_wanted ]
 
 dip = data.matrix(diploid[,seq(1,ncol(diploid),INTERVAL) ])
-#dip_var = colVars(dip)
-#s = sort(dip_var,decreasing = TRUE)
-#dip_reduce = dip[,which(dip_var > 0)]
+hip = data.matrix(half_hap[,seq(1,ncol(half_hap),INTERVAL) ])
 
+
+### COMPUTATION TASKS ###
+
+## pca
 task = "compute pca"
 start = Sys.time()
 pca = prcomp(dip)
-#pca = nsprcomp(dip,nrestart = 1,em_maxiter = 50,ncomp = 3,k = 100) 
 total = Sys.time() - start
 print(task)
 print(total)
 
-#legend(x="topright", legend = levels(as.factor(g$data$groups)), col=c("red","blue","green"), pch=1)
-#pcc = pca.xyz(dip)
-#a = project.pca(dip,pcc)
 
+## kmeans for whole genome
 task = "kmeans for whole genome"
 start = Sys.time()
 bb = kmeans(diploid,4)
@@ -111,12 +184,13 @@ total = Sys.time() - start
 print(task)
 print(total)
 
-#plain plot
+## plain plot
 plot((pca$sdev^2/sum(pca$sdev^2))[1:10],type = "o",xlab = "Principal Components",ylab = "Varaince Explained", main = "Scree Plot")
 plot(pca$x[,1],pca$x[,2],xlab = "PC1", ylab = "PC2", main = "PC1 vs PC2")
 pairs(pca$x[,1:3])
 plot(pca$x[,1],pca$x[,2],col= c("black","red","blue","green")[as.factor(global_populations)])
 
+## kmeans for PCA data
 task = "kmeans for projected data"
 start = Sys.time()
 bbb = kmeans(pca$x[,1:4],4,nstart = 5) 
@@ -128,6 +202,8 @@ bbb = kmeans(pca$x[,1:5],4,nstart = 5)
 clusplot(pca$x[,2:3],bbb$cluster,color = TRUE, shade = TRUE,lines = FALSE,main = "" )
 
 #write.csv(pca$x[,1:4],sprintf("./data/chr-%s.geno.pca.csv",CHROMOSOME_NUMBER),row.names = FALSE)
+
+## attempt with hierachical clustering did not work
 if (FALSE){
   #heirarchical clustering
   dissimilarity = dist(pca$x[,1:5])
@@ -139,7 +215,7 @@ if (FALSE){
   plot(hcd)
 }
 
-#determining the number of clusters
+## Determining the number of clusters
 kmax = 15
 wss = sapply(1:kmax,function(k){kmeans(pca$x[,1:5],k,nstart = 10)$tot.withinss})
 plot(1:kmax, wss,
@@ -151,15 +227,23 @@ fviz_nbclust(pca$x[,1:4], hcut, method = "silhouette",hc_method = "complete") #t
 fviz_nbclust(pca$x[,2:3], kmeans, method = "silhouette") #this one gives us three
 
 
-#sparse
-pca = nsprcomp(dip,nrestart = 10,em_maxiter = 100,ncomp = 3,k = 300)
+## Sparse pca
+pca = nsprcomp(hip,nrestart = 10,em_maxiter = 100,ncomp = 3,k = 300)
 reduced_snps = union(union(which(pca$rotation[,1] != 0),which(pca$rotation[,2] != 0)),which(pca$rotation[,3] != 0))
 
+## Mixture modeling
+output = mixture(hip[,reduced_snps],K=4)
+clustering = apply(output$Rik,1,which.max)
+table(clustering,global_populations)
+ 
 #prediction time
-population_names = dimnames(table(global_populations))$global_populations #make this better
+population_names = dimnames(table(global_populations))$global_populations 
 indicies = sapply(population_names,function(x){which(global_populations == x)})
 datum = pca_total
+F1score(clustering,global_populations)
 
+
+## kfold cross validation
 average_f1score = 0
 num_folds = 10
 for (i in c(1:num_folds)){
@@ -172,7 +256,7 @@ for (i in c(1:num_folds)){
 }
 print(average_f1score/num_folds)
 
-#all of the pcas into one
+## all of the pcas into one
 pca_22 = read.csv(sprintf("./data/chr-%s.geno.pca.csv",22))
 pca_21 = read.csv(sprintf("./data/chr-%s.geno.pca.csv",21))
 pca_20 = read.csv(sprintf("./data/chr-%s.geno.pca.csv",20))
@@ -185,7 +269,7 @@ plot(dependent,good,type ="b",ylim = c(.73,.95),col = "blue",ylab = "F1 score",x
 lines(dependent,bad,type = "b", col = "red")
 legend(x="topright", legend = c("Using PCA","Whole Chromsome") , col=c("blue","red"), pch=1)
 
-#time benchmark
+## time benchmark
 Pca_seconds = c(281,174,169)
 kmeans_seconds = c(714)
 plot(dependent,Pca_seconds,type ="b",ylim = c(100,800),col = "blue",ylab = "Run time (seconds)",xlab = "Chromosome")
